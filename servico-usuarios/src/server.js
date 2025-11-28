23d47c5305c79fa935e0162737d74c4639b4a869
Carlos Kolody <130325636+Carlos-Kolody@users.noreply.github.com>
2025-11-28 20:54:47 -0300

    Delete servico-usuarios directory

--- servico-usuarios/src/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-
-// Define a porta (3002 é a porta que definimos para usuários no README)
-const PORT = process.env.PORT || 3002;
-
-app.listen(PORT, () => {
-    console.log(`Servidor de Usuários rodando em http://localhost:${PORT}`);
-});
