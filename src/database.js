692c32e452dd699962182838cf13c50bc1f4f87f
Carlos Kolody <130325636+Carlos-Kolody@users.noreply.github.com>
2025-11-28 20:54:52 -0300

    Delete src directory

--- src/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize')
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite', // O arquivo será criado na raiz
-  logging: false
-})
-
-module.exports = sequelize
